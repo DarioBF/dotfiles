862eee77bede29117492ff9be7de4a0b4f4675e7
DarioBF <contacto@dariobf.com>
2024-05-14 13:58:17 +0200

    updates

--- desktop/.config/sway/scripts/clamshell.sh
+++ desktop/.config/sway/scripts/clamshell.sh
@@ -1,12 +1,41 @@
-#!/bin/sh
+#!/bin/bash
+
+# SETTINGS
+# ============
+
+# Values: off | on | log
+DEBUG="off"
+DEBUG_COMMANDS=0
 
 LAPTOP_OUTPUT="eDP-1"
-LID_STATE_FILE="/proc/acpi/button/lid/LID0/state"
+BACKGROUND_IMAGE="/home/dariobf/.config/sway/wallpapers/purple_landscape.jpg"
 
-read -r LS < "$LID_STATE_FILE"
+# HELPERS
+# ============
 
-case "$LS" in
-*open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
-*)       echo "Could not get lid state" >&2 ; exit 1 ;;
-esac
+log() {
+    [[ "$DEBUG" != "off" ]] && echo $1
+}
+
+[[ $DEBUG == "log" ]] && exec >> /tmp/clamshell.log 2>&1
+[[ $DEBUG_COMMANDS -eq 1 ]] && set -x
+
+# START
+# ============
+
+log "Script invocation: $0 $*"
+
+active_outputs=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name')
+only_laptop=$(expr "${active_outputs}" == "${LAPTOP_OUTPUT}")
+
+log "Active outputs: $active_outputs"
+log "Only Laptop?    $only_laptop"
+
+if [ "$1" = "on" ]; then
+    swaymsg output "$LAPTOP_OUTPUT" disable
+    if [ $only_laptop -eq 1 ]; then
+        /usr/bin/swaylock -f -i "$BACKGROUND_IMAGE" && /usr/bin/systemctl suspend
+    fi
+elif [ "$1" = "off" ]; then
+    swaymsg output "$LAPTOP_OUTPUT" enable
+fi
--- desktop/.config/sway/scripts/lock_and_suspend.sh
+++ desktop/.config/sway/scripts/lock_and_suspend.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-swaylock -f -c 000000 && systemctl suspend
+exec > /tmp/lock_and_suspend.log 2>&1
+set -x
+
+LAPTOP_OUTPUT="eDP-1"
+
+# Función para comprobar si solo eDP-1 está activo
+is_only_laptop_output_active() {
+    # Obtener el número de salidas activas
+    active_outputs=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name')
+    count_active_outputs=$(echo "$active_outputs" | wc -l)
+    
+    # Comprobar si solo hay una salida activa y si es eDP-1
+    if [ "$count_active_outputs" -eq 1 ] && [ "$active_outputs" == "$LAPTOP_OUTPUT" ]; then
+        return 0  # True
+    else
+        return 1  # False
+    fi
+}
+
+# Comprobar si solo está activo eDP-1 y ejecutar swaylock y systemctl suspend si es así
+if is_only_laptop_output_active; then
+    /usr/bin/swaylock -f -i "/home/dariobf/.config/sway/wallpapers/purple_landscape.jpg" -c 000000 && /usr/bin/systemctl suspend
+fi
