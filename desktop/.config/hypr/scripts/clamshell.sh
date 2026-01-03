cc79bb7ebc0648f9d612aa57f1bdddbf9e307365
DarioBF <contacto@dariobf.com>
2026-01-03 19:11:04 +0100

    Added DMS lock to suspend

--- desktop/.config/hypr/scripts/clamshell.sh
+++ desktop/.config/hypr/scripts/clamshell.sh
@@ -154,7 +154,8 @@ if [ "$1" = "close" ]; then
 
   if [ "$only_laptop" -eq 1 ]; then
     log "Only laptop output active, locking and suspending."
-    swaylock -f -i "$BACKGROUND_IMAGE" && systemctl suspend
+    # swaylock -f -i "$BACKGROUND_IMAGE" && systemctl suspend
+    dms ipc call lock lock
   fi
 
 elif [ "$1" = "open" ]; then
